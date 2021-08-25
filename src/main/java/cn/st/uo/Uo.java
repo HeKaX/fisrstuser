94c97d6f1755f924d8a14f851ebed0c38a106452
hk <1823653596@qq.com>
2021-08-25 14:19:11 +0800

    next

--- src/main/java/cn/st/uo/Uo.java
+++ src/main/java/cn/st/uo/Uo.java
@@ -5,6 +5,8 @@ public class Uo {
 	
 	private void sysou() {
 		// TODO Auto-generated method stub
-
+				sysou();
+				System.out.println("bbb");
+				System.out.println("aaaa");
 	}
 }
---
+++ src/main/java/cn/st/uo/Vi.java
@@ -0,0 +1,10 @@
+package cn.st.uo;
+
+public class Vi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("aaa");
+	}
+
+}
